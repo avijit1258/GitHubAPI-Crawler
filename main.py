1ecd1b8242a9f5b2d0c40aa8a21843027097aecb
Avijit Bhattacharjee <avb307@barley.usask.ca>
2020-07-02 11:08:51 -0600

    checking cross repo for same organization and printing type of repo1 and repo2

--- main.py
+++ main.py
@@ -29,11 +29,24 @@ def mine_cross_reference_pr_issues_parallel(repo, pr_ids):
     return 
 
 def getting_single_issue(id):
+    type_repo1 = 'ISSUE'
+    type_repo2 = 'ISSUE'
     issue_events = api.get_issue_pr_timeline(repo, id)
     for event in issue_events:
-        if event['event'] == 'cross-referenced' and repo not in event['source']['issue']['html_url']:
-            print('https://github.com/', repo, '/issues/', id, ' , ' , event['source']['issue']['html_url'], sep='')
+        if event['event'] == 'cross-referenced' and repo not in event['source']['issue']['html_url'] and is_from_same_organization(repo, event['source']['issue']['html_url']):
+            type_repo1 = 'ISSUE' if 'issues' in repo else 'PR'
+            type_repo2 = 'ISSUE' if 'issues' in event['source']['issue']['html_url'] else 'PR'
+            print('https://github.com/', repo, '/issues/', id, ' , ', type_repo1 , ' , ' , event['source']['issue']['html_url'], ',', type_repo2, sep='')
     return
+
+def is_from_same_organization(repo1, repo2):
+
+    # print(repo1.split('/'))
+    if repo1.split('/')[3] == repo2.split('/')[3]:
+        # print('True')
+        return True
+
+    return False
 
 def get_source_of_cross_reference(repo, issue_id):
     # 33773 is a cross-reference event
@@ -54,9 +67,12 @@ if __name__ == "__main__":
     # query issue/pr timeline
     # events = api.get_issue_pr_timeline("jquery/jquery", 4406)
 
+    # is_from_same_organization('https://github.com/facebook/react/issues/14981', 'https://github.com/facebook/redux-toolkit/issues/331')
+
     with open('data/repoList_morethan200PR.txt') as f:
         repos = [line.rstrip() for line in f]
     repos = random.sample(repos, 10)
+    repos.append('nodejs/node')
     print(repos)
     for r in repos:
         repo = r
